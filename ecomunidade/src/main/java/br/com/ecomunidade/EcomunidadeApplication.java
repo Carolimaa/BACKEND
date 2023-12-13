abba55d8e33c30ffd585eae5dd40a6502b1d0322
Vinicius Lima <vinicius.slima99@icloud.com>
2023-12-13 11:29:28 -0300

    Backend

---
+++ ecomunidade/src/main/java/br/com/ecomunidade/EcomunidadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecomunidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomunidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomunidadeApplication.class, args);
+	}
+
+}
---
+++ ecomunidade/src/test/java/br/com/ecomunidade/EcomunidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ecomunidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomunidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

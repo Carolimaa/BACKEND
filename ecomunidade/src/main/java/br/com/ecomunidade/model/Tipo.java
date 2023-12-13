1631cc8bb8868ef1430f6aa33caec920461766d6
Gakuraa <gabrieliwakura924@gmail.com>
2023-12-13 11:42:22 -0300

    Adição da model tipo

---
+++ ecomunidade/src/main/java/br/com/ecomunidade/model/Tipo.java
@@ -0,0 +1,69 @@
+package br.com.ecomunidade.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_tipo")
+public class Tipo {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private Long id;
+	
+	@NotNull
+	@NotBlank
+	@Column(length = 50)
+	private String nome;
+	
+	@NotNull
+	@Column(length = 150)
+	private String descricao;
+	
+	@UpdateTimestamp
+	private LocalDateTime data;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+	
+}
